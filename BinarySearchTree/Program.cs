a4c21bd6ec49db31746af73852e8c6f06f25934a
“preetimca2016” <preetimca2016@gmail.com>
2022-08-29 13:47:09 +0530

    Add:UC3-For Searchnode

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -9,6 +9,7 @@ namespace BinarySearchTreeNameSpace
             Console.WriteLine("Binary tree Search");
             Console.WriteLine("Enter 1 for AddNode");
             Console.WriteLine("Enter 2 for GetSizeOfTree");
+            Console.WriteLine("Enter 3 SearchNode");
             BinarySearchTree<int> BinaryTree = new BinarySearchTree<int>();
             int Option = Convert.ToInt32(Console.ReadLine());
 
@@ -22,6 +23,10 @@ namespace BinarySearchTreeNameSpace
                 case 2:
                     int Size = BinaryTree.GetSize();
                     Console.WriteLine(Size);
+                    break;
+                case 3:
+                    bool result = BinaryTree.SearchKey(63);
+                    Console.WriteLine(result);
                     break;
 
             }
